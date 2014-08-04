0ca7656eeed25c8cfd0179f5c45b1bd4ede1bda6
Oded Welgreen <oded_wel@hotmail.com>
2014-08-04 22:51:22 +0300

    Add ICell

---
+++ DunGen.Engine/Contracts/ICell.cs
@@ -0,0 +1,12 @@
+﻿using DunGen.Engine.Models;
+
+namespace DunGen.Engine.Contracts
+{
+    public interface ICell
+    {
+        int Row { get; set; }
+        int Column { get; set; }
+        TerrainType Terrain { get; set; }
+
+    }
+}
