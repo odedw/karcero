3d57c4d58b6557ee312d4bebef8715e8bbdde2f3
Oded Welgreen <oded_wel@hotmail.com>
2014-09-20 22:16:56 +0300

    Try to embed unity in demo page

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -2,18 +2,63 @@
  * Created by oded on 30/08/2014.
  */
 $(function(){
+    mixpanel.track('Page View - Karcero Demo');
+
+    var keys = ['size','randomness','sparseness','deadends','room-size','room-count'];
     function methodChanged(){
-        var definitions = {
-            'size': $('#size').val(),
-            'randomness': $('#randomness').val(),
-            'sparseness': $('#sparseness').val(),
-            'deadends': $('#deadends').val(),
-            'room-size': $('#room-size').val(),
-            'room-count': $('#room-count').val()
+        var definitions = {};
+        for(var i=0;i<keys.length;i++){
+            definitions[keys[i]] = $('#'+keys[i]).val();
         }
     };
-    mixpanel.track('Page View - Karcero Demo');
     $(".api-select").dropkick({
         change:methodChanged
     });
+    setTimeout(function(){
+
+        var config = {
+            width: $('.wrapper').width(),
+            height: $('.wrapper').width(),
+            params: { enableDebugging:"1" }
+        };
+        console.log($('.wrapper').css('width'));
+        var u = new UnityObject2(config);
+
+        var $missingScreen = jQuery("#unityPlayer").find(".missing");
+        var $brokenScreen = jQuery("#unityPlayer").find(".broken");
+        $missingScreen.hide();
+        $brokenScreen.hide();
+
+        u.observeProgress(function (progress) {
+            switch(progress.pluginStatus) {
+                case "broken":
+                    $brokenScreen.find("a").click(function (e) {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        u.installPlugin();
+                        return false;
+                    });
+                    $brokenScreen.show();
+                    break;
+                case "missing":
+                    $missingScreen.find("a").click(function (e) {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        u.installPlugin();
+                        return false;
+                    });
+                    $missingScreen.show();
+                    break;
+                case "installed":
+                    $missingScreen.remove();
+                    break;
+                case "first":
+                    break;
+            }
+        });
+        u.initPlugin(jQuery("#unityPlayer")[0], "/karcero/content/Unity.unity3d");
+    });
+
 });
+
+
