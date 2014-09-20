35f50e2923904881f78d48ecef8a481df20f9858
Oded Welgreen <oded_wel@hotmail.com>
2014-09-20 17:36:50 +0300

    Add unity related files

---
+++ Karecor.UnityDemo/Assets/Scripts/Controller.cs
@@ -0,0 +1,41 @@
+﻿using Karcero.Engine;
+using Karcero.Engine.Models;
+using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+
+    private DungeonGenerator<Cell> mGenerator;
+
+    // Use this for initialization
+    void Start()
+    {
+        mGenerator = new DungeonGenerator<Cell>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mGenerator.GenerateA()
+                      .MediumDungeon()
+                      .ABitRandom()
+                      .SomewhatSparse()
+                      .WithMediumChanceToRemoveDeadEnds()
+                      .WithMediumSizeRooms()
+                      .WithLargeNumberOfRooms()
+                      .AndTellMeWhenItsDone(map =>
+               {
+                   Debug.Log("Worked! room count - " + map.Rooms.Count);
+
+               });
+        }
+    }
+
+    void OnMouseDown()
+    {
+
+    }
+}
